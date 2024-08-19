7e27faacae1ab0060e1703fd4c5728cc9462500a
mudassir-arif <mudassir@aptechgdn.net>
2024-08-19 11:29:25 +0500

    issue resolved

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    // mongodb://localhost:27017/crimereport
     try {
-        await mongoose.connect('mongodb://localhost:27017/crimereport', {
+        // await mongoose.connect('mongodb+srv://mudassirinoxent:amftFleiRItjlUr7@cluster0.2rtvxcg.mongodb.net/enotebook', {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true,
+        // });
+        await mongoose.connect('mongodb+srv://crime-report:qJ9ccIUXESS3quQ4@cluster0.uzc7q.mongodb.net/crime', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
